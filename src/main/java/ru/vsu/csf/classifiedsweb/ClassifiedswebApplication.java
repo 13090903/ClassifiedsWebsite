f8932c3efdfa8fd1f6ddd2c582029187bdb62f6b
Dmitry <iamdimonis@gmail.com>
2024-02-24 12:59:58 +0300

    initial commit

---
+++ src/main/java/ru/vsu/csf/classifiedsweb/ClassifiedswebApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.csf.classifiedsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassifiedswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassifiedswebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vsu/csf/classifiedsweb/ClassifiedswebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.csf.classifiedsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassifiedswebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
